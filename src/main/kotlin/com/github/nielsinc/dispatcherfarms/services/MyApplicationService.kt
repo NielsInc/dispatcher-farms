de328d616e91b8268905e46e2ae0d33b79f87f89
GitHub Action <action@github.com>
2022-03-02 09:44:07 +0000

    Template cleanup

--- src/main/kotlin/com/github/nielsinc/dispatcherfarms/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/nielsinc/dispatcherfarms/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.nielsinc.dispatcherfarms.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.nielsinc.dispatcherfarms.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/nielsinc/dispatcherfarms/services/MyProjectService.kt
+++ src/main/kotlin/com/github/nielsinc/dispatcherfarms/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.nielsinc.dispatcherfarms.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.nielsinc.dispatcherfarms.MyBundle
 
 class MyProjectService(project: Project) {
 
